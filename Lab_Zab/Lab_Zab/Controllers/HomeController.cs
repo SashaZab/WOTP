c4f797c576dfd4663836cfd0a44df38f187c4b22
SashaZabavskyi <Sanok201511@gmail.com>
2021-12-13 22:09:31 +0200

    Lab3

--- Lab_Zab/Lab_Zab/Controllers/HomeController.cs
+++ Lab_Zab/Lab_Zab/Controllers/HomeController.cs
@@ -7,6 +7,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Services.Email_service;
+using Lab_Zab.Models.Home;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 
 namespace Lab_Zab.Controllers
 {
@@ -14,15 +18,23 @@ namespace Lab_Zab.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public HomeController(ILogger<HomeController> logger, IEmailSender emailSender)
+        public HomeController(ILogger<HomeController> logger, IEmailSender emailSender, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
             _emailSender = emailSender;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public IActionResult Index()
         {
+            var currentUrl = _httpContextAccessor.HttpContext.Request.GetEncodedUrl();
+            // Получение имени компьютера.
+            String host = Dns.GetHostName();
+            // Получение ip-адреса.
+            IPAddress ip = Dns.GetHostByName(host).AddressList[0];
+            _logger.LogInformation($"Url: {currentUrl}, Time: {DateTime.Now}, IP Address: {ip}");
             return View();
         }
 
@@ -35,6 +47,13 @@ namespace Lab_Zab.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+        public async Task<IActionResult> SendEmail([FromForm] EmailMessageVM vm)
+        {
+            await _emailSender.SendMessage(vm.EmailTo, vm.MessageBody, vm.Subject);
+            return Json(new { success = true });
         }
     }
 }
--- Lab_Zab/Lab_Zab/Startup.cs
+++ Lab_Zab/Lab_Zab/Startup.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Services.Email_service;
+using Microsoft.Extensions.Logging;
 
 namespace Lab_Zab
 {
@@ -26,11 +27,13 @@ namespace Lab_Zab
         {
             services.AddControllersWithViews();
             services.AddTransient<IEmailSender, EmailSender>();
+            services.AddHttpContextAccessor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddFile(Configuration.GetSection("Logging"));
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

abb24d3982dafb6bb68c708f9b2ddce8388fd011
SashaZabavskyi <Sanok201511@gmail.com>
2021-12-13 16:00:38 +0200

    lab1
    lab1

---
+++ Lab_Zab/Lab_Zab/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_Zab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

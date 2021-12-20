7a60318ac24c2d3552e1e5b69166e9ebed2a31f9
SashaZabavskyi <Sanok201511@gmail.com>
2021-12-20 23:36:35 +0200

    Lab4
    Lab4

---
+++ Lab_Zab/Lab_Zab/Models/File/UploadFileViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Lab_Zab.Models.File
+{
+    public class UploadFileViewModel
+    {
+        public string Name { get; set; }
+        public IFormFile File { get; set; }
+    }
+}

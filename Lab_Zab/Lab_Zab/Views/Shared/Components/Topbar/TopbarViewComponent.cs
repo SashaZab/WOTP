9578eafa9c0e9969b205b38973594626a0927e65
SashaZabavskyi <Sanok201511@gmail.com>
2021-12-21 12:00:33 +0200

    Lab6
    Lab6

---
+++ Lab_Zab/Lab_Zab/Views/Shared/Components/Topbar/TopbarViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Lab_Zab.Views.Shared.Components.Topbar
+{
+    public class TopbarViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            TopbarViewModel vm = new TopbarViewModel()
+            {
+                IsAdminUser = true
+            };
+            return View(vm);
+        }
+    }
+}
+
